caf7b08677ee6e534361973665930abe36083e1f
samjy <sam+git@samjy.com>
2015-12-14 18:13:44 +0100

    Testing, and lots of fixes and improvements

--- sloot/object/obj_utils.py
+++ sloot/object/obj_utils.py
@@ -22,9 +22,6 @@ class dictobj(dict):
     def __init__(self, *args, **kwargs):
         """Initializing
         """
-        self._changed_values = {}
-        self._initialized = True
-
         dicts = []
         for arg in args:
             if not isinstance(arg, dict):
@@ -36,35 +33,112 @@ class dictobj(dict):
             for k, v in list(dic.items()):
                 if isinstance(v, dict):
                     v = dictobj(v)
+
                 self[k] = v
+
+        self._changed_values = {}
+        self._deleted_keys = set()
+        self._initialized = True
 
     def __setitem__(self, key, value):
         """Setting items the dict way...
         """
-        self._changed_values[key] = value
+        if hasattr(self, '_initialized'):
+            self._deleted_keys.discard(key)
+            self._changed_values[key] = value
+
         super(dictobj, self).__setitem__(key, value)
 
-    def __getattr__(self, item):
-        """Maps values to attributes.
-        Only called if there *isn't* an attribute with this name
+    def __delitem__(self, name):
+        """Delete a dict key
         """
-        try:
-            return self.__getitem__(item)
-        except KeyError:
-            raise AttributeError("%s not found in %s" % (item, self.__class__.__name__))
+        if hasattr(self, '_initialized'):
+            self._deleted_keys.add(name)
+            if name in self._changed_values:
+                del self._changed_values[name]
+
+        super(dictobj, self).__delitem__(name)
+
+    def update(self, *args, **kwargs):
+        """Update the dict
+        """
+        if hasattr(self, '_initialized'):
+            self._changed_values.update(*args, **kwargs)
+            self._deleted_keys -= set(self._changed_values.keys())
+
+        return super(dictobj, self).update(*args, **kwargs)
+
+    def setdefault(self, key, default=None):
+        """Set default
+        """
+        willchange = key not in self
+        ret = super(dictobj, self).setdefault(key, default)
+        if hasattr(self, '_initialized'):
+            if willchange:
+                self._changed_values[key] = ret
+
+        return ret
+
+    def clear(self):
+        """Clear
+        """
+        if hasattr(self, '_initialized'):
+            self._changed_values.clear()
+            self._deleted_keys |= set(self.keys())
+
+        return super(dictobj, self).clear()
+
+    def __getattribute__(self, name):
+        """Get attribute
+
+        - if it's on the object/class, we'll get it from there
+        - otherwise, try to get it from the dict
+
+        NOTE: the object/class attributes take precedence over the dict
+
+        :returns: The attribute
+        :raises: (AttributeError) If not there
+        """
+        dct = dict.__getattribute__(self, '__dict__')
+        cls = dict.__getattribute__(self, '__class__')
+        if name in dct or hasattr(cls, name) or '_initialized' not in dct:
+            # try to get the standard attribute (method, variable, etc) if it's
+            # present on the class or we're not initialized
+            return dict.__getattribute__(self, name)
+        else:
+            # when trying to get an unknown attribute, get it from the dict
+            try:
+                return self[name]
+            except:
+                # we didn't find the attribute
+                pass
+
+        raise AttributeError("%s not found in %s" % (name, cls.__name__))
 
     def __setattr__(self, item, value):
         """Maps attributes to values.
         Only if we are initialised
+
+        NOTE: to avoid issues, we don't allow to setattr on class attributes
+              (e.g. to avoid modifying dict.items function)
         """
         if '_initialized' not in self.__dict__:
             # this test allows attributes to be set in the __init__ method
             return dict.__setattr__(self, item, value)
         elif item in self.__dict__:
             # any normal attributes are handled normally
-            dict.__setattr__(self, item, value)
-        else:
-            self.__setitem__(item, value)
+            return dict.__setattr__(self, item, value)
+
+        return self.__setitem__(item, value)
+
+    def __delattr__(self, name):
+        """Delete attribute
+        """
+        if name in self.__dict__ or hasattr(self.__class__, name):
+            # any normal attributes are handled normally
+            return dict.__delattr__(self, name)
+
+        return self.__delitem__(name)
 
 
 class SimpleMultiDict(dict):
--- tests/test_obj_utils.py
+++ tests/test_obj_utils.py
@@ -4,9 +4,11 @@
 """
 """
 
+import sys
 import unittest
+import traceback
 import sloot.object.obj_utils as obj_utils
-from sloot.object import dictobj
+from sloot.object import dictobj, SimpleMultiDict
 import collections
 
 
@@ -14,7 +16,7 @@ class TestMyobject(unittest.TestCase):
     """Testing myobject
     """
 
-    def test_init(self):
+    def test___init__(self):
         """Testing initialisation
         """
         o = obj_utils.myobject(test1="test1", test2="test2")
@@ -25,6 +27,263 @@ class TestMyobject(unittest.TestCase):
 class TestDictobj(unittest.TestCase):
     """Testing dictobject
     """
+
+    def test___init__(self):
+        """Testing __init__
+        """
+        # all ways to init a dict work
+        # keyword arguments
+        t = dictobj(a='a', b='b')
+        self.assertEqual(t, {'a': 'a', 'b': 'b'})
+        # a dict
+        t = dictobj({'a': 'a', 'b': 'b'})
+        self.assertEqual(t, {'a': 'a', 'b': 'b'})
+        # an iterable
+        t = dictobj([('a', 'a'), ('b', 'b')])
+        self.assertEqual(t, {'a': 'a', 'b': 'b'})
+        # several dicts
+        t = dictobj({'a': 'a'}, {'b': 'b'})
+        self.assertEqual(t, {'a': 'a', 'b': 'b'})
+
+        # if some values are dict, they're changed to dictobj too
+        t = dictobj({'a': {'b': 'b'}})
+        self.assertTrue(isinstance(t.a, dictobj))
+
+        # and it's recursive
+        t = dictobj({'a': {'b': {'c': 'c'}}})
+        self.assertTrue(isinstance(t.a.b, dictobj))
+
+        # circular-referencing dicts are not supported
+        orig = {}
+        orig['orig'] = orig
+        self.assertRaises(RuntimeError, dictobj, orig)
+
+    def test___setitem__(self):
+        """Testing __setitem__
+        """
+        t = dictobj()
+        self.assertEqual(t._changed_values, {})
+
+        t = dictobj({'a': 'a', 'b': 'b', 'c': 'c'})
+
+        t.e = 'e'
+        t['f'] = 'f'
+        self.assertEqual(t._changed_values, {'e': 'e', 'f': 'f'})
+
+        t.f = "new f"
+        self.assertEqual(t._changed_values, {'e': 'e', 'f': "new f"})
+
+    def test___delitem___(self):
+        """Testing __delitem__
+        """
+        t = dictobj({'a': 'a', 'b': 'b'})
+
+        # delitem on 'a' key
+        del t['a']
+        self.assertEqual(t, {'b': 'b'})
+        self.assertEqual(t._deleted_keys, set('a'))
+
+        # changed values is updated if needed
+        t = dictobj()
+        t.a = 'a'
+        t.b = 'b'
+        self.assertEqual(t._changed_values, {'a': 'a', 'b': 'b'})
+        del t['a']
+        self.assertEqual(t._deleted_keys, set('a'))
+        self.assertEqual(t._changed_values, {'b': 'b'})
+
+    def test_update(self):
+        """Testing update
+        """
+        t = dictobj()
+        t.update({'a': 'a', 'b': 'b', 'c': 'c'})
+        self.assertEqual(t._changed_values, {'a': 'a', 'b': 'b', 'c': 'c'})
+
+    def test_setdefault(self):
+        """Testing setdefault
+        """
+        t = dictobj()
+        t.setdefault('a', 'a')
+        self.assertEqual(t.a, 'a')
+        self.assertEqual(t._changed_values, {'a': 'a'})
+
+    def test_clear(self):
+        """Testing clear
+        """
+        t = dictobj({'a': 'a', 'b': 'b'})
+        t.clear()
+        self.assertEqual(t._changed_values, {})
+        self.assertEqual(t._deleted_keys, set(['a', 'b']))
+
+    def test___getattribute__(self):
+        """Testing __getattribute__
+        """
+        class T(dictobj):
+
+            @property
+            def b(self):
+                raise KeyError('b')
+
+            @property
+            def c(self):
+                raise AttributeError('c')
+
+            @property
+            def d(self):
+                raise ValueError('d')
+
+            @property
+            def e(self):
+                raise RuntimeError('e')
+
+            @property
+            def f(self):
+                return "f"
+
+            @property
+            def h(self):
+                # something here raises an attribute error
+                return self.c
+
+
+        t = T(a='a')
+
+        self.assertEqual(t.a, 'a')
+        self.assertRaises(KeyError, lambda: t.b)
+        self.assertRaises(AttributeError, lambda: t.c)
+        self.assertRaises(ValueError, lambda: t.d)
+        self.assertRaises(RuntimeError, lambda: t.e)
+        self.assertEqual(t.f, 'f')
+        self.assertRaises(AttributeError, lambda: t.g)
+
+        raised = False
+        try:
+            x = t.h
+        except AttributeError:
+            raised = True
+            stk = traceback.extract_tb(sys.exc_info()[2])
+            # we don't hide the original attribute error
+            self.assertEqual(stk[-1][3], "raise AttributeError('c')")
+
+        assert raised
+
+    def test___getattribute__precedence(self):
+        class T(dictobj):
+            a = "class a"
+
+            def __init__(self, *args, **kwargs):
+                self.b = 'object b'
+                super(T, self).__init__(*args, **kwargs)
+
+            def c(self):
+                return "function c"
+
+            @property
+            def d(self):
+                return "property d"
+
+        t = T()
+
+        self.assertTrue('a' not in t)
+        self.assertTrue('b' not in t)
+        self.assertTrue('c' not in t)
+        self.assertTrue('d' not in t)
+
+        self.assertEqual(t.a, "class a")
+        self.assertEqual(t.b, "object b")
+        self.assertTrue(callable(t.c))
+        self.assertEqual(t.d, "property d")
+
+        t.update({'a': 'a',
+                  'b': 'b',
+                  'c': 'c',
+                  'd': 'd'})
+
+        # now we have elts in the dict
+        self.assertTrue('a' in t)
+        self.assertTrue('b' in t)
+        self.assertTrue('c' in t)
+        self.assertTrue('d' in t)
+
+        # properties still go to the class/object elements
+        self.assertEqual(t.a, "class a")
+        self.assertEqual(t.b, "object b")
+        self.assertTrue(callable(t.c))
+        self.assertEqual(t.d, "property d")
+
+    def test___setattr__(self):
+        """Testing __setattr__
+        """
+        class T(dictobj):
+            def __init__(self, *args, **kwargs):
+                self.test = 'test'
+                super(T, self).__init__(*args, **kwargs)
+
+        t = T({'a': 'a', 'b': 'b'})
+        self.assertEqual(t, {'a': 'a', 'b': 'b'})
+
+        # changed existing item
+        t.a = "aaa"
+        self.assertEqual(t, {'a': 'aaa', 'b': 'b'})
+
+        # new item
+        t.c = "c"
+        self.assertEqual(t, {'a': 'aaa', 'b': 'b', 'c': 'c'})
+
+        # object attr
+        self.assertEqual(t.test, "test")
+        t.test = "the test"
+        self.assertEqual(t.test, "the test")
+        self.assertEqual(t, {'a': 'aaa', 'b': 'b', 'c': 'c'})
+
+        self.assertEqual(t.__dict__, {
+            'test': 'the test',
+            '_changed_values': {'a': 'aaa', 'c': 'c'},
+            '_deleted_keys': set([]),
+            '_initialized': True,
+        })
+
+    def test___setattr__class_attribute(self):
+        """Testing __setattr__ for class attribute
+        """
+        class T(dictobj):
+            a = "class a"
+
+        t = T()
+
+        # class attribute: setattr goes to the dict, and not the class
+        # attribute
+        self.assertEqual(t.a, "class a")
+        self.assertTrue('a' not in t)
+        t.a = "a"
+        self.assertTrue('a' in t)
+        self.assertEqual(t, {'a': 'a'})
+        self.assertEqual(t.a, "class a")
+
+    def test___delattr__(self):
+        """Testing __delattr__
+        """
+        t = dictobj({'a': 'a', 'b': 'b'})
+
+        # delitem on 'a' key
+        del t.a
+        self.assertEqual(t, {'b': 'b'})
+        self.assertEqual(t._deleted_keys, set('a'))
+
+        class T(dictobj):
+            def __init__(self, *args, **kwargs):
+                self.test = 'test'
+                super(T, self).__init__(*args, **kwargs)
+
+        t = T()
+        self.assertEqual(t.test, "test")
+        self.assertEqual(t, {})
+
+        # delattr on 'test' attribute
+        del t.test
+        self.assertFalse(hasattr(t, 'test'))
+        self.assertEqual(t, {})
+        self.assertEqual(t._deleted_keys, set())
 
     def test_globally(self):
         """Testing dictobj
@@ -74,6 +333,31 @@ class TestDictobj(unittest.TestCase):
         assert t.bar == "world"
 
         print(t.__dict__)
+
+
+class TestSimpleMultiDict(unittest.TestCase):
+    """Testing SimpleMultiDict
+    """
+
+    def test_getlist(self):
+        """Testing getlist
+        """
+        d = SimpleMultiDict({'a': 'a', 'b': 'b'})
+        # put in a list if not a list
+        self.assertEqual(d.getlist('a'), ['a'])
+        self.assertEqual(d.getlist('b'), ['b'])
+
+        # if already a list, returned unchanged
+        d['a'] = ['a', 'aa', 'aaa']
+        self.assertEqual(d.getlist('a'), ['a', 'aa', 'aaa'])
+
+    def test___repr__(self):
+        """Testing __repr__
+        """
+        d = SimpleMultiDict({'a': ['a', 'aa', 'aaa'], 'b': 'b'})
+        self.assertEqual(
+            repr(d),
+            "SimpleMultiDict({'a': ['a', 'aa', 'aaa'], 'b': 'b'})")
 
 
 if __name__ == '__main__':
