5d3473f38cfa7f04ca32a9d9cc79c7ec3bd84241
samjy <sam+git@samjy.com>
2019-06-25 18:53:20 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@
 from setuptools import setup, find_packages
 from sloot.object import __VERSION__
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="sloot.object",
     version=__VERSION__,
@@ -17,6 +20,8 @@ setup(
     author="Samuel Jaillet",
     author_email="sam+dev@samjy.com",
     description="Object utils to make life easier",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     license="MIT",
     keywords=['tools', 'object', 'utils', 'dict'],
     url="https://github.com/samjy/sloot.object",
